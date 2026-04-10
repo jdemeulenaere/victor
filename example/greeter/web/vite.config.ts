cae0ef619c2c0b9ebf7f536c5488c2989cb3fbf0
Jordan Demeulenaere <j.demeulenaere1@gmail.be>
2026-04-10 22:01:40 +0200

    Generate typescript client

--- example/greeter/web/vite.config.ts
+++ example/greeter/web/vite.config.ts
@@ -1,9 +1,15 @@
+import path from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   root: process.cwd(),
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@bufbuild/protobuf': path.resolve(process.cwd(), 'node_modules/@bufbuild/protobuf/dist/esm'),
+    },
+  },
   server: {
     proxy: {
       '/grpc': {
