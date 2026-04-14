30d39049fedcc2fe175b56b61e9b61bc1288dead
Jordan Demeulenaere <j.demeulenaere1@gmail.be>
2026-04-14 06:58:24 +0200

    Remove decorated multiplatform library and move desktop greeter path

--- example/kotlin/multiplatform/GreetingDecorator.android.kt
+++
@@ -1,5 +0,0 @@
-package victor.example.multiplatform
-
-actual object GreetingDecorator {
-    actual val suffix: String = " [from Android library 2]"
-}
--- example/kotlin/multiplatform/GreetingDecorator.jvm.kt
+++
@@ -1,5 +0,0 @@
-package victor.example.multiplatform
-
-actual object GreetingDecorator {
-    actual val suffix: String = " [from JVM library 2]"
-}
