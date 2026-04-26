81e5be45aba1c938f982d02dff0025d8142d8e69
Jordan Demeulenaere <j.demeulenaere1@gmail.com>
2026-04-26 18:20:50 +0200

    Add iOS sample unit test support

---
+++ src/samples/greeter/ios/GreeterAppTest.swift
@@ -0,0 +1,22 @@
+import XCTest
+@testable import VictorGreeterIOS
+
+final class GreeterAppTest: XCTestCase {
+    @MainActor
+    func testCallSayHelloShowsResponse() async {
+        let model = GreeterViewModel(client: StubGreeterClient())
+
+        model.name = "iOS test"
+        model.callSayHello()
+        await Task.yield()
+
+        XCTAssertEqual(model.responseMessage, "Hello, iOS test! (from iOS test)")
+        XCTAssertFalse(model.loading)
+    }
+}
+
+private final class StubGreeterClient: GreeterServing {
+    func sayHello(name: String, completion: @escaping (Result<String, Error>) -> Void) {
+        completion(.success("Hello, \(name)! (from iOS test)"))
+    }
+}
