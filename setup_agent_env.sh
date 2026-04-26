32a15e26371c4b2c471a90f931abfa90b3f27965
Jordan Demeulenaere <j.demeulenaere1@gmail.com>
2026-04-26 11:59:13 +0200

    Add script to setup cloud agents environment

---
+++ setup_agent_env.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# Set up the environment to be used by AI agents (Codex Cloud, Jules, etc).
+
+# Install bazel.
+sudo apt install apt-transport-https curl gnupg -y
+curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg
+sudo mv bazel-archive-keyring.gpg /usr/share/keyrings
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+sudo apt update && sudo apt install bazel-9.0.1
+sudo ln -s /usr/bin/bazel-9.0.1 /usr/bin/bazel
+
+# Set up BuildBuddy remote caching.
+cat >> ~/.bazelrc << EOF
+common --bes_results_url=https://app.buildbuddy.io/invocation/
+common --bes_backend=grpcs://remote.buildbuddy.io
+common --remote_cache=grpcs://remote.buildbuddy.io
+common --noremote_upload_local_results
+common --remote_timeout=10m
+common --remote_header=x-buildbuddy-api-key=$BUILDBUDDY_API_KEY
+EOF
+
+# Build and test.
+./build.sh
+./test.sh
